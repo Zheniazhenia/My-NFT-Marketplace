5b57d3bd0f79003f96da2c29829e84154d38a1fb
zheniazhenia <cptyevgenii@ukr.net>
2022-11-15 16:20:38 +0200

    Purchase Contract is done

--- src/backend/contracts/Marketplace.sol
+++ src/backend/contracts/Marketplace.sol
@@ -29,6 +29,16 @@ contract Marketplace is ReentrancyGuard {
         address indexed seller
     ); // indexed means that we can search like w filters
 
+    event Bought (
+        uint itemId,
+        address indexed nft, 
+        uint tokenId,
+        uint price,
+        address indexed seller,
+        address indexed buyer
+    );
+    
+
     // mapping is key->value like itemId -> Item
      mapping(uint => Item) public items;
 
@@ -36,7 +46,7 @@ contract Marketplace is ReentrancyGuard {
         feeAccount = payable(msg.sender);
         feePercent = _feePercent;
     }
-        function makeItem(IERC721 _nft, uint _tokenId, uint _price) external nonReentrant {
+    function makeItem(IERC721 _nft, uint _tokenId, uint _price) external nonReentrant {
         require(_price > 0, "Price must be greater than zero");
         // increment itemCount
         itemCount ++;
@@ -59,5 +69,30 @@ contract Marketplace is ReentrancyGuard {
             _price,
             msg.sender
         );
+    }
+    function purchaseItem(uint _itemId) external payable nonReentrant {
+        uint _totalPrice = getTotalPrice(_itemId);
+        Item storage item = items[_itemId];
+        require(_itemId > 0 && _itemId <= itemCount, "item doesnt exist");
+        require(msg.value >= _totalPrice, "not enough ether to cover item price and market fee");
+        require(!item.sold, "item already sold");
+        // pay seller and feeAccount
+        item.seller.transfer(item.price);
+        feeAccount.transfer(_totalPrice - item.price);
+        // update item to sold
+        item.sold = true;
+        // transfer nft to buyer
+        item.nft.transferFrom(address(this), msg.sender, item.tokenId);
+        emit Bought(
+            _itemId,
+            address(item.nft),
+            item.tokenId,
+            item.price,
+            item.seller,
+            msg.sender
+        );
+    }
+    function getTotalPrice(uint _itemId) view public returns(uint) {
+        return(items[_itemId].price*(100 + feePercent));
     }
 }
